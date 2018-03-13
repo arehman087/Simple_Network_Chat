1d3637f53c3de17a1ff9602b7840ec604d16692b
Abdul Rehman <abudrehman45@gmail.com>
2018-03-12 22:26:03 -0500

    implement coroutine in the client

--- Client/Client.py
+++ Client/Client.py
@@ -1,6 +1,19 @@
+import asyncio
+import enum
 import logging
-import socket
 import pickle
+
+
+class MessageType(enum.Enum):
+    """
+    byte sent at beginning of the message to specify the type
+    """
+    ON_CONNECT = 0
+    ON_DISCONNECT = 1
+    MESSAGE = 2
+    WHISPER = 3
+    SHOUT = 4
+    RESEND_NAME = 5
 
 
 class Client(object):
@@ -8,37 +21,43 @@ class Client(object):
     creates a client that will recieve messages
     """
 
-    def __init__(self):
-        self.__sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.__name = "Abdul"
-        self.__sock.connect(('localHost', 4444))
+    def __init__(self, reader, writer, loop):
+        self.__reader = reader
+        self.__writer = writer
+        self.__loop = loop
+
+    def send_name(self):
+        ######NEED TO FIX
+        x = input('What is your name?')
+        self.__writer.write(bytes([0, 0]))
+
+        self.__writer.write(pickle.dumps(x))
 
     def converse(self):
-        logging.debug("talking to server")
+        pass
 
-        try:
-            message = input('what would you like to send? ')
-            pickleled_data = pickle.dumps(message)
-            self.__sock.sendall(pickleled_data)
 
-            while True:
-                rec_data = self.__sock.recv(4096)
-                data = pickle.loads(rec_data)
-                print ("server sent: "+ str(data))
+async def cl_functions(loop):
+    try:
+        reader, writer = await asyncio.open_connection(host='localHost', port=4444, loop=loop)
+    except ConnectionResetError:
+        logging.error("ConnectionResetError")
+        return
+    except asyncio.streams.IncompleteReadError:
+        logging.error("asyncio.streams.IncompleteReadError")
+        return
+    except OSError:
+        logging.error("OSError")
+        return
 
-                message = input('what would you like to send? ')
-                pickleled_data = pickle.dumps(message)
-                self.__sock.sendall(pickleled_data)
+    client = Client(reader, writer, loop)
+    client.send_name()
 
-        except KeyboardInterrupt:
-            pass
-
-        finally:
-            self.__sock.close()
 
 def main():
-    client = Client
-    client.converse()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(cl_functions(loop))
+
 
 if __name__ == '__main__':
     main()
